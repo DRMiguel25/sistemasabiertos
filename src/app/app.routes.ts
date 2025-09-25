f5dc9e5016e1fff684bd4107c05691665fe86996
DRMiguel25 <l.dirm040525@itses.edu.mx>
2025-09-25 07:38:53 -0600

    Ejercicio Heroe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './counter/counter-page.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { HeroPageComponent } from './pages/hero/hero-page/hero-page.component';
 
 export const routes: Routes = [
   {
@@ -10,7 +11,9 @@ export const routes: Routes = [
     path: 'usuario', component: UsuarioComponent
   },
   {
+    path: 'hero', component: HeroPageComponent
+  },
+  {
     path: '', redirectTo: 'usuario', pathMatch: 'full' 
   }
 ];
-
---
+++ src/app/pages/hero/hero-page/hero-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  styleUrls: ['./hero-page.component.scss']
+})
+export class HeroPageComponent {
+  name = signal('Ironman');
+  age = signal(45);
+
+  getHeroDescription() {
+    return `${this.name()} - ${this.age()}`;
+  }
+
+  changeHero() {
+    this.name.set('Spiderman');
+    this.age.set(22);
+  }
+
+  changeAge() {
+    this.age.set(60);
+  }
+
+  resetForm() {
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+}
