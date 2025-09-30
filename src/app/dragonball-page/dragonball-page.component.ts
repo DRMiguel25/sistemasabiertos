3cfb3782d752e04e9b5edbbc63f827971e642685
DRMiguel25 <l.dirm040525@itses.edu.mx>
2025-09-30 08:06:37 -0600

    señales

--- src/app/dragonball-page/dragonball-page.component.ts
+++ src/app/dragonball-page/dragonball-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 interface Character {
   id: number;
@@ -10,14 +11,31 @@ interface Character {
 @Component({
   selector: 'app-dragonball-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dragonball-page.component.html',
   styleUrls: ['./dragonball-page.component.scss']
 })
 export class DragonballPageComponent {
+  nombre = '';
+  power = 0;
+
   characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 9001},
-    {id: 3, name: 'Krilin', power: 9001}
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8001 },
+    { id: 3, name: 'Krilin', power: 1001 },
+    { id: 4, name: 'Yamcha', power: 500 }
   ]);
+
+  agregar() {
+    if (this.nombre.trim() && this.power > 0) {
+      const newCharacter: Character = {
+        id: this.characters().length + 1,
+        name: this.nombre.trim(),
+        power: this.power
+      };
+      this.characters.set([...this.characters(), newCharacter]);
+      this.nombre = '';
+      this.power = 0;
+    }
+  }
 }
